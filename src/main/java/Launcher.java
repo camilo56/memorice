10b30d06efa956d8855e4360fdf9aeabebdbd03f
camilo56 <76082597+camilo56@users.noreply.github.com>
2021-09-23 17:56:32 -0300

    uso del metodo star() para inciar el hilo ubicado en  run()

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,7 +1,9 @@
 public class Launcher {
     public static void main(String[] args) {
         String tableros[][];
-        Memorice memorice =new Memorice();
+        MemoriceThread memorice =new MemoriceThread();
+
+        memorice.start();// el metodo start() es para iniciar el codigo en otro hilo que este dentro del metodo run() en la clase MemoriceThread
         memorice.menu();
     }
 }
