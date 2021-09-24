1d2a93b693b092b99e36d9228190cd4f639d7a82
camilo56 <76082597+camilo56@users.noreply.github.com>
2021-09-24 20:06:45 -0300

    cambios en torno a Tableros dinamicos

--- src/main/java/Tableros.java
+++ src/main/java/Tableros.java
@@ -68,7 +68,6 @@ public class Tableros {
             for (int i = 0; i < matriz.length; i++) {
                 for (int j = 0; j < matriz[i].length; j++) {
                     System.out.print("[" + matriz[i][j] + "]");
-
                 }
                 System.out.println("");
             }
