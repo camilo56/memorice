1cf46fac0387cc3d8901aec46b388bdabb89a12f
prain01 <71102083+prain01@users.noreply.github.com>
2021-10-31 15:44:12 -0300

    implementacion de creacion de archivos csv

--- src/main/java/Clases_Dominio/Estadisticas.java
+++ src/main/java/Clases_Dominio/Estadisticas.java
@@ -1,5 +1,7 @@
 package Clases_Dominio;
+import Manejo_Archivos.GestorArchivo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Estadisticas {
@@ -11,7 +13,53 @@ public class Estadisticas {
     public Estadisticas(String nick){
         this.persona = new Persona(nick);
         this.vecesJugadas = getVecesJugadas();
-        this.puntajes = getPuntajes();
+        this.puntajes = new ArrayList<>();
+    }
+
+    public void ingresarPuntaje(Integer puntaje){
+        puntajes.add(puntaje);
+    }
+
+    public Boolean guardarEstadisticas() {
+        GestorArchivo gestorArchivo = new GestorArchivo();
+        String ruta = "Estadisticas.csv";
+        try {
+            if(!gestorArchivo.existeArchivo(ruta)){
+                gestorArchivo.crearArchivo(ruta);
+                gestorArchivo.escribirEnArchivo(ruta, crearEstructuraArchivo());
+            }
+            gestorArchivo.escribirEnArchivo(ruta, crearDatos());
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al guardar datos");
+            e.printStackTrace();
+            return  false;
+        }
+    }
+
+    /*
+    private String crearDatos(){
+        String datos = "Nick: " + getPersona().getNick() + "; Veces Jugadas: " + getVecesJugadas();
+        for(Integer puntaje : getPuntajes()){
+            datos += "; " + puntaje;
+        }
+        return datos;
+    }
+     */
+    private String crearEstructuraArchivo(){
+        String nombresColumnas = "Nick;Veces Jugadas";
+        for (int i = 0; i < vecesJugadas; i++) {
+            nombresColumnas += ";Puntaje Jugada Nº" + (i + 1);
+        }
+        return  nombresColumnas;
+    }
+
+    private String crearDatos(){
+        String datos = getPersona().getNick() + ";" + getVecesJugadas();
+        for(Integer puntaje : getPuntajes()){
+            datos += ";" + puntaje;
+        }
+        return datos;
     }
 
     public Persona getPersona() {
