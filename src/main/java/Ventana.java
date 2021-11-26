063556adef3f1e1c84848b3687aaeb6f7d1056ab
Sebastian <s.calfunao01@ufromail.cl>
2021-11-26 01:07:14 -0300

    etiqueta...

--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -12,9 +12,21 @@ public class Ventana extends JFrame {
     private void iniciarComponentes(){
         JPanel panel = new JPanel();
         //panel.setBackground(Color.CYAN);
+        panel.setLayout(null);//desactivando el diseño
         this.getContentPane().add(panel);
+
+
         JLabel etiqueta = new JLabel();
         etiqueta.setText("HOLA"); // establecemos al texto de la etiqueta
+        etiqueta.setBounds(20,20,100,40);
+        etiqueta.setHorizontalAlignment(SwingConstants.CENTER);//Estableze la alineasion horizontal del texto
+        etiqueta.setForeground(Color.blue); //establecemos el color de la letra
+        etiqueta.setOpaque(true);//establecemos pintar el fondo de la etiqueta
+        etiqueta.setBackground(Color.yellow);//cambiamos el color del fondo de la etiqueta
+        etiqueta.setFont(new Font("arial",Font.ITALIC,20));//establemos la fuente del texto
         panel.add(etiqueta); //agregamos la etiqueta al panel
+
+
+        //etiqueta 2-etiqueta tipo imagen
     }
 }
